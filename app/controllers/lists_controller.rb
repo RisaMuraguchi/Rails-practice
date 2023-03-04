a7b5a65bcde233f68b8c3d471a0711b3fb021002
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-04 12:30:02 +0000

    3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,11 +3,11 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  def create  
+  def create
     list = List.new(list_params)
     list.save
     redirect_to list_path(list.id)
-  end  
+  end
 
   def index
     @lists = List.all
@@ -20,16 +20,22 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end  
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-end  
+end
